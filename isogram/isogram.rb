d8d7c5de3c7e1e5c6aa5eff091f9a5fd2edd3106
AgustinPalmaM <palma.agustin7@gmail.com>
2022-04-07 18:24:36 -0400

    isogram exercise done

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,7 +1,12 @@
-=begin
-Write your code for the 'Isogram' exercise in this file. Make the tests in
-`isogram_test.rb` pass.
+# Write your code for the 'Isogram' exercise in this file. Make the tests in
+# `isogram_test.rb` pass.
+#
+# To get started with TDD, see the `README.md` file in your
+# `ruby/isogram` directory.
 
-To get started with TDD, see the `README.md` file in your
-`ruby/isogram` directory.
-=end
+module Isogram
+  def self.isogram?(input)
+    new_input = input.downcase.gsub('-', '').gsub(' ', '')
+    new_input.length == new_input.split('').uniq.length
+  end
+end
